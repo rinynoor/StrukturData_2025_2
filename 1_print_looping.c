6407ea0f83ddf2249d931321dec6368420c2b234
Riny Noor <riny91@gmail.com>
2026-02-12 17:26:45 +0700

    Add looping to print numbers from 1 to 5
    Prints numbers 1 to 5 using a loop.

---
+++ 1_print_looping.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+
+    printf("\nMenggunakan looping:\n");
+
+    for(int i = 1; i <= 5; i++){
+        printf("%d ", i);
+    }
+
+    return 0;
+}
